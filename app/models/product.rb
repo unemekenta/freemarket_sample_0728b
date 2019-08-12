55f0806b50f6fe71e1fd31e18987dbf8d5cc00dd
kamayatsu <50704071+kamayatsu@users.noreply.github.com>
2019-08-13 02:15:41 +0900

    Update product.rb
    has_many :product_image"s"に修正
    他テーブルに値を保存するために、accepts_nested_attributes_forを記載

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,9 +4,10 @@ has_many :users, through: :messages
 has_many :users, through: :todoes
 has_many :users, through: :transactions
 has_many :users, through: :purchases
-has_many :product_image
+has_many :product_images
 has_one :category
 has_one :status
 has_one :condition
+accepts_nested_attributes_for :product_images
 has_one :delivery
 end
