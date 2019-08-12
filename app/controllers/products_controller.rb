9ecba7dfe8e246db455257e983025ac3bf5f624f
kamayatsu <50704071+kamayatsu@users.noreply.github.com>
2019-08-13 02:17:37 +0900

    Edit new action & Create create action
    newアクションに他テーブル用の要素を追加
    createアクションとprivateメソッドを作成し、フォームからパラメータを受け取って保存する機能を構築

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,5 +4,19 @@ class ProductsController < ApplicationController
 
   def new
     @product = Product.new
+    10.times { @product.product_images.build }
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to root_path
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :price, :detail, :status_id, :condition_id, product_images_attributes: [:id, :image])
   end
 end
