1dd70cd50278a0a5951f86a69af2db7754e94294
kamayatsu <50704071+kamayatsu@users.noreply.github.com>
2019-08-12 17:07:12 +0900

    Write Associations
    アソシエーションを記載

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,12 @@
 class Product < ApplicationRecord
-  
+has_many :users, through: :likes
+has_many :users, through: :messages
+has_many :users, through: :todoes
+has_many :users, through: :transactions
+has_many :users, through: :purchases
+has_many :product_image
+has_one :category
+has_one :status
+has_one :condition
+has_one :delivery
 end
