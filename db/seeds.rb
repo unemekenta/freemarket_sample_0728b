bfb287bd1421182fa9993271278eb4c020741f9c
YuichiMotomura <yu1liveinwellisle@gmail.com>
2019-08-13 18:45:39 +0900

    Delete_seeds.rb
    実装用の仮データを入れるためのseedsファイルの内容を削除

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-50.times do |i|
-  Product.create(name: "【新品未使用】PATRICK COX (パトリックコックス) シューズ 黒", price: 808976, detail: "【新品未使用】PATRICK COX (パトリックコックス) シューズ 黒", status_id: 2, condition_id: 4, created_at: "2019-08-12", updated_at: "2019-08-12")
-  ProductImage.create(image: "https://static.mercdn.net/thumb/photos/m80932943959_1.jpg?1565686566", product_id: i , created_at: "2019-08-12", updated_at: "2019-08-12")
-end
