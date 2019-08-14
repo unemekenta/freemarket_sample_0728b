15b990307f00f583b0aa73262bf72405111bde6c
kamayatsu <50704071+kamayatsu@users.noreply.github.com>
2019-08-14 20:37:14 +0900

    Create form deliver_region
    deliver_region tableに保存するためのカラムを作成。
    のちにactive hashで実装予定だが、仮置きでこの形式で作成。

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -18,6 +18,6 @@ class ProductsController < ApplicationController
   private
 
   def product_params
-    params.require(:product).permit(:name, :price, :detail, :status_id, :condition_id, delivery_attributes: [:id, :shipping_fee, :deliver_method_id, :estimated_date_id], product_images_attributes: [:id, :image])
+    params.require(:product).permit(:name, :price, :detail, :status_id, :condition_id, delivery_attributes: [:id, :shipping_fee, :deliver_method_id, :estimated_date_id, :deliver_region_id], product_images_attributes: [:id, :image])
   end
 end
