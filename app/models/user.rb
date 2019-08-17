973cac4cb5516b3cea7ce4c05d196c762837c578
uneme <unemekenta@gmail.com>
2019-08-17 23:17:36 +0900

    fix_models

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_one :deliver_address
+  has_many :address
+  accepts_nested_attributes_for :deliver_address, :address
 end
