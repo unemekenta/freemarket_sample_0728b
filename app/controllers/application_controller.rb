2f0055a060cdabc5e1f1ca235e8c7436dc736b20
uneme <unemekenta@gmail.com>
2019-08-18 14:38:00 +0900

    fix_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,9 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :family_name, :first_name, :family_name_pseudonym, :first_name_pseudonym, :birthday, :photo, :phone_number, address_attributes: [:post_number]])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :family_name, :first_name, :family_name_pseudonym, :first_name_pseudonym, :birthday, :photo, :phone_number])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [address_attributes: [:id, :post_number, :prefecture, :city, :street, :building]])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [deliver_address_attributes: [:id, :post_number, :prefecture, :city, :street, :building, :phone_number]])
   end
 
   private
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,12 +2,10 @@ class UsersController < ApplicationController
   def new
     @user = User.new
     @user.build_deliver_address
-    @user.addresses.build
+    @user.build_address
   end
   
   def create
-    
-    binding.pry
     
     @user = User.new(user_params)
     if @user.save
@@ -15,6 +13,7 @@ class UsersController < ApplicationController
     end
   end
 
+  private
   def user_params
     params.require(:user).permit(:family_name, :first_name, :family_name_pseudonym, :first_name_pseudonym, :birthday, :nickname, :photo, :email, :password, :phone_number, deliver_address_attributes: [:id, :post_number, :prefecture, :city, :street, :building, :phone_number], address_attributes: [:id, :post_number, :prefecture, :city, :street, :building])
   end
