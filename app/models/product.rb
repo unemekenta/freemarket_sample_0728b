054dbff0e902bd6cdfd5d5425f7b72115a570ccc
HIROKUNI <tc4tonchan@gmail.com>
2019-08-24 13:19:19 +0900

    Update product.rb
    added
    dependent: :destroy and allow_destroy: true

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,12 +4,12 @@ class Product < ApplicationRecord
   has_many :users, through: :todoes
   has_many :users, through: :transactions
   has_many :users, through: :purchases
-  has_many :product_images
+  has_many :product_images, dependent: :destroy
   belongs_to :category
   belongs_to :brand, optional: true
   belongs_to :size, optional: true
   belongs_to :status
   belongs_to :condition
-  has_one :delivery
-  accepts_nested_attributes_for :product_images, :delivery
+  has_one :delivery, dependent: :destroy
+  accepts_nested_attributes_for :product_images, :delivery, allow_destroy: true
 end
