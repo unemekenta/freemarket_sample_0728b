b000ccc847bde500befc4a031cf91e25bf2bea96
uneme <unemekenta@gmail.com>
2019-08-25 17:12:43 +0900

    set_routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'products/index'
+  get 'products/pay'
   devise_for :users, controllers: { registrations: 'users/registrations' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'products#index'
