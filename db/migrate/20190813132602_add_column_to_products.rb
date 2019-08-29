1b822ac9a512a77f4cd65d716a7f8be30a9e5ecc
YuichiMotomura <yu1liveinwellisle@gmail.com>
2019-08-29 16:21:13 +0900

    Modify migration file
    productテーブル、カラム追加時に、8/13のマイグレーションファイルと同名になり、上書きしたことが分かったので、ブランチ作成前の状態にリセット

--- db/migrate/20190813132602_add_column_to_products.rb
+++ db/migrate/20190813132602_add_column_to_products.rb
@@ -1,5 +1,5 @@
 class AddColumnToProducts < ActiveRecord::Migration[5.2]
   def change
-    add_reference :products, :seller, foreign_key: {to_table: :users}
+    add_reference :products, :delivery, null: false, foreign_key: true
   end
 end
--- db/migrate/20190827090407_remove_column_to_products.rb
+++
@@ -1,5 +0,0 @@
-class RemoveColumnToProducts < ActiveRecord::Migration[5.2]
-  def change
-    remove_columns :products, :seller_id, :seller
-  end
-end
