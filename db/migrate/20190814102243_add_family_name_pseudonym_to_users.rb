6d9948a70ed2790ed1d793baf5a2e5bef47fbe93
Masatomo Sugai <tech.masatomo.s@gmail.com>
2019-09-04 15:54:34 +0900

    add 'not null'
    not null制約が抜けていたため追加

--- db/migrate/20190814102243_add_family_name_pseudonym_to_users.rb
+++ db/migrate/20190814102243_add_family_name_pseudonym_to_users.rb
@@ -1,5 +1,5 @@
 class AddFamilyNamePseudonymToUsers < ActiveRecord::Migration[5.2]
   def change
-    add_column :users, :family_name_pseudonym, :string
+    add_column :users, :family_name_pseudonym, :string, null: false
   end
 end
