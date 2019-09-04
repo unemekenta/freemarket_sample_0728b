c156fcdaddb3be774dd3135026c4aac58d5067f3
Masatomo Sugai <tech.masatomo.s@gmail.com>
2019-09-04 17:04:06 +0900

    add from_omniauth, new_with_session
    debiseへの追記
    メソッドの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,24 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, 
+         :omniauthable
 
   has_one :address
   has_one :deliver_address
   accepts_nested_attributes_for :address, :deliver_address
+
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first
+  end
+
+  def self.new_with_session(_, session)
+    super.tap do |user|
+      if (data = session['devise.omniauth_data'])
+        user.email = data['email'] if user.email.blank?
+        user.nickname = data['name'] if user.nickname.blank?
+      end
+    end
+  end
 end
