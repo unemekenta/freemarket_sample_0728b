03bae0993e9413899c718e2f85c28c7e231927de
Masatomo Sugai <tech.masatomo.s@gmail.com>
2019-09-05 14:31:05 +0900

    fix user_controller
    SNS経由の場合で分岐するように修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -43,10 +43,13 @@ class UsersController < ApplicationController
       first_name: session[:first_name],
       family_name_pseudonym: session[:family_name_pseudonym],
       first_name_pseudonym: session[:first_name_pseudonym],
-      provider: session["devise.omniauth_data"]['provider'],
-      uid: session["devise.omniauth_data"]['uid']
-
     )
+
+    if session["devise.omniauth_data"].present?
+      @user[:provider] = session["devise.omniauth_data"]['provider']
+      @user[:uid] = session["devise.omniauth_data"]['uid']
+    end
+
     @user.build_address(user_params[:address_attributes])
     if @user.save
       session[:id] = @user.id
