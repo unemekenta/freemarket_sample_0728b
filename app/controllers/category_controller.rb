ea75527cbf90dd6abdb3c035769568b9b0450b75
Masatomo Sugai <tech.masatomo.s@gmail.com>
2019-09-07 11:14:09 +0900

    create show method at Category controller
    category controllerにshowメソッドを追記

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -1,2 +1,12 @@
 class CategoryController < ApplicationController
+
+  def show
+    @category = Category.find(params[:id])
+    @categories = [
+      @category,
+      @category.children,
+      @category.children.map { |category| category.children }
+    ].flatten.compact
+    @products = Product.where(category_id: @categories).where.not(status_id: 4).order("id DESC").limit(50)
+  end
 end
