2c71965dac3fbc8624895e34b5621685a5a12d55
uneme <unemekenta@gmail.com>
2019-09-08 16:37:13 +0900

    fix_conflicts

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,26 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, 
+         :omniauthable
 
   has_one :address
-  has_one :deliver_address
-  accepts_nested_attributes_for :address, :deliver_address
+  has_one :deliveraddress
+  accepts_nested_attributes_for :address
+  has_many :products, through: :purchases
+  has_many :purchases
   has_many :credit_cards
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first
+  end
+
+  def self.new_with_session(_, session)
+    super.tap do |user|
+      if (data = session['devise.omniauth_data'])
+        user.email = data['email'] if user.email.blank?
+        user.nickname = data['name'] if user.nickname.blank?
+      end
+    end
+  end
 end
