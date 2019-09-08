efe320ade8953605f42adf71aba9a1c5f874f137
uneme <unemekenta@gmail.com>
2019-09-08 12:13:07 +0900

    fix_controller

--- app/controllers/purchase_controller.rb
+++ app/controllers/purchase_controller.rb
@@ -20,9 +20,9 @@ class PurchaseController < ApplicationController
     card = CreditCard.where(user_id: current_user.id).first
     Payjp.api_key = ENV['PAYJP_SECRET_KEY']
     Payjp::Charge.create(
-    :amount => 13500, #支払金額を入力（itemテーブル等に紐づけても良い）
-    :customer => card.customer_id, #顧客ID
-    :currency => 'jpy', #日本円
+    amount: @product.price, #支払金額を入力（itemテーブル等に紐づけても良い）
+    customer: card.customer_id, #顧客ID
+    currency: 'jpy', #日本円
   )
   redirect_to action: 'done' #完了画面に移動
   end
