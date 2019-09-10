99a793f337ed96c0bac8c1ab650c943bce9d7916
kamayatsu <50704071+kamayatsu@users.noreply.github.com>
2019-09-11 02:20:58 +0900

    Create product_image_spec.rb
    初期段階として、2つのテストを導入
    - product_imageのproduct_idカラムがnullではない事
    - callback(after_update)がきちんと呼び出されている事

---
+++ spec/models/product_image_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe ProductImage, type: :model do
+  describe '#new, #edit' do
+    it "is invalid without a product_id" do
+      product_image = build(:product_image, product_id: "")
+      product_image.valid?
+      expect(product_image.errors[:product]).to include("must exist")
+    end
+  end
+
+  context 'callbacks' do
+    it { is_expected.to callback(:delete_records).after(:update).if :image_changed? }
+  end
+end
