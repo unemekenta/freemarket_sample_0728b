ab51fd3d39d83c1e72548e3916deb04b574e1521
HIROKUNI <tc4tonchan@gmail.com>
2019-09-12 12:34:54 +0900

    Create user.rb

---
+++ spec/factories/users.rb
@@ -0,0 +1,12 @@
+FactoryBot.define do
+  factory :user do
+    nickname                    {"K"}
+    birthday                    {"1998-04-01"}
+    family_name                 {"山田"}
+    first_name                  {"太郎"}
+    family_name_pseudonym       {"ヤマダ"}
+    first_name_pseudonym        {"タロウ"}
+    email                       {"test@user"}
+    password                    {"000000"}
+  end
+end
