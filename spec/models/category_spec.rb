4f55b6661229ec1e00a46dfe673215b55ee47fd9
Masatomo Sugai <tech.masatomo.s@gmail.com>
2019-09-12 20:54:14 +0900

    Create category test
    初期段階として、カテゴリー名を呼び出せるかをテストしました。
    親、子、孫まで関連付けて呼び出せるかのテストは違うブランチで行います。

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
-RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Category do
+  describe 'Category #show' do
+    it "Categoryテーブルにデータを保存できる" do
+      category = build(:category)
+      expect(category).to be_valid
+    end
+
+    it "Categoryテーブルに保存されたブランド名を呼び出せる" do
+      category = build(:category)
+      expect(category.category).to eq "テストカテゴリー"
+    end
+  end
 end
+
+
+
