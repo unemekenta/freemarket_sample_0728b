c560a3be8861b2cb810c20f0aba27a034f5b4fdd
YuichiMotomura <yu1liveinwellisle@gmail.com>
2019-09-13 21:50:42 +0900

    Update show.html.haml
    redirect_toをprefixに書き換え

--- app/controllers/credit_card_controller.rb
+++ app/controllers/credit_card_controller.rb
@@ -4,14 +4,14 @@ class CreditCardController < ApplicationController
 
   def new
     card = CreditCard.where(user_id: current_user.id)
-    redirect_to action: "show" if card.exists?
+    redirect_to user_credit_card_path(current_user, card) if card.exists?
     
   end
 
   def pay #payjpとCardのデータベース作成を実施します。
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
     if params['payjp-token'].blank?
-      redirect_to action: "new"
+      redirect_to new_user_credit_card_path(current_user)
     else
       customer = Payjp::Customer.create(
       description: '登録テスト', #なくてもOK
@@ -21,9 +21,9 @@ class CreditCardController < ApplicationController
       ) #念の為metadataにuser_idを入れましたがなくてもOK
       @card = CreditCard.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
       if @card.save
-        redirect_to action: "show", user_id: current_user, id: @card
+        redirect_to user_credit_card_path(current_user, @card)
       else
-        redirect_to action: "pay"
+        redirect_to pay_user_credit_card_index_path(current_user)
       end
     end
   end
@@ -37,7 +37,7 @@ class CreditCardController < ApplicationController
       customer.delete
       card.delete
     end
-      redirect_to ({action: 'show', user_id: current_user, id: 0}), notice: 'カードを削除しました'
+      redirect_to user_credit_card_path(current_user, 0), notice: 'カードを削除しました'
   end
 
   def show #Cardのデータpayjpに送り情報を取り出します
