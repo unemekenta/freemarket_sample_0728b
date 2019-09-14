3c88531d8e95ac9ba8097be89e5041e3056e50d3
kamayatsu <50704071+kamayatsu@users.noreply.github.com>
2019-09-15 05:24:04 +0900

    Create products.rb
    基本項目を作成し、associationを追記

---
+++ spec/factories/products.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do 
+  factory :product do
+    id {"1"}
+    name {"test"}
+    price {"1000"}
+    detail {"it is a test for product model"}
+    seller_id {"1"}
+
+    association :condition
+    association :status
+    association :category
+    association :brand
+  end
+end
