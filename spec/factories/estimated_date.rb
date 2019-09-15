6228ef842343459453151052554e4fdeb3d5137a
uneme <unemekenta@gmail.com>
2019-09-15 20:04:55 +0900

    set_factories_estimated_date

---
+++ spec/factories/estimated_date.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+
+    factory :estimated_date do
+      id    {"1"}
+      date {"1~2日"}
+      created_at                  {"2019-09-09"}
+      updated_at                  {"2019-09-09"}
+    end
+  
+  end
