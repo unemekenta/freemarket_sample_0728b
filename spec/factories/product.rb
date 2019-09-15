b0b13fe5f82b33b3a81d671088b8a06e4a0a3c9a
uneme <unemekenta@gmail.com>
2019-09-15 20:05:09 +0900

    set_factories_product

---
+++ spec/factories/product.rb
@@ -0,0 +1,18 @@
+FactoryBot.define do
+
+    factory :product do
+      id    {"1"}
+      name {"服"}
+      price {"5000"}
+      detail {"綺麗な品です"}
+      created_at                  {"2019-09-09"}
+      updated_at                  {"2019-09-09"}
+      status_id {"1"}
+      condition_id {"1"}
+      brand_id {"1"}
+      category_id {"1"}
+      size_id {"1"}
+      seller_id {"1"}
+    end
+  
+  end
