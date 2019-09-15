32c53f8ee6f7647aee5a4d6423927e1c6f7f3727
uneme <unemekenta@gmail.com>
2019-09-15 20:04:17 +0900

    set_factorier_method_delivery

---
+++ spec/factories/deliver_method.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+
+    factory :deliver_method do
+        id    {"1"}
+        method {"1"}
+        created_at                  {"2019-09-09"}
+        updated_at                  {"2019-09-09"}
+      end
+  
+end
