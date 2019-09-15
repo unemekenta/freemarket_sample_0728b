5cf36fe01bae63ecb4b2841b9e0a9d6d1873ad28
YuichiMotomura <yu1liveinwellisle@gmail.com>
2019-09-15 15:48:11 +0900

    Update size_type_spec.rb
    size_typesモデルのテストを実施。size_typesと結びつくcategoriesとsizesのアソシエーションに問題ないか確認。

--- spec/models/size_type_spec.rb
+++ spec/models/size_type_spec.rb
@@ -1,5 +1,25 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe SizeType, type: :model do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
+RSpec.describe SizeType, type: :model do
+  describe 'size_typeモデルはcategoriesとsizesと適切なアソシエーションを組めているか' do
+    
+      let(:size_type) { FactoryBot.create(:size_type) }
+      let(:category1) { FactoryBot.create(:category, category: 'カテゴリ1', size_type: size_type) }
+      let(:category2) { FactoryBot.create(:category, category: 'カテゴリ2', size_type: size_type) }
+      let(:size1) { FactoryBot.create(:size, size: "size1", size_type: size_type) }
+      let(:size2) { FactoryBot.create(:size, size: "size2", size_type: size_type) }
+    
+    it 'size_typesとcategoriesは1対多のアソシエーションである1' do
+      expect(category1.size_type).to eq size_type
+    end
+    it 'size_typesとcategoriesは1対多のアソシエーションである2' do
+      expect(size_type.categories).to eq [category1, category2]
+    end
+    it 'size_typesとsizesは1対多のアソシエーションである1' do
+      expect(size1.size_type).to eq size_type
+    end
+    it 'size_typesとsizesは1対多のアソシエーションである2' do
+      expect(size_type.sizes).to eq [size1, size2]
+    end
+  end
+end
