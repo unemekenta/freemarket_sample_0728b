3f621035ab192289d960a80fbf968447abf76ec6
YuichiMotomura <yu1liveinwellisle@gmail.com>
2019-09-15 19:14:02 +0900

    Modify size_type_spec.rb
    アソシエーションをテストファイルに明示するのではなく、factoryBot側で設定する

--- spec/factories/categories.rb
+++ spec/factories/categories.rb
@@ -2,6 +2,6 @@ FactoryBot.define do
   factory :category do
     category {"テストカテゴリー1"}
     parent_id {"1"}
-    # association :size_type
+    association :size_type
   end
 end
--- spec/models/size_type_spec.rb
+++ spec/models/size_type_spec.rb
@@ -4,22 +4,27 @@ RSpec.describe SizeType, type: :model do
   describe 'size_typeモデルはcategoriesとsizesと適切なアソシエーションを組めているか' do
     
       let(:size_type) { FactoryBot.create(:size_type) }
-      let(:category1) { FactoryBot.create(:category, category: 'カテゴリ1', size_type: size_type) }
-      let(:category2) { FactoryBot.create(:category, category: 'カテゴリ2', size_type: size_type) }
-      let(:size1) { FactoryBot.create(:size, size: "size1", size_type: size_type) }
-      let(:size2) { FactoryBot.create(:size, size: "size2", size_type: size_type) }
+      let(:category1) { FactoryBot.create(:category, category: 'カテゴリ1') }
+      let(:category2) { FactoryBot.create(:category, category: 'カテゴリ2') }
+      let(:size1) { FactoryBot.create(:size, size: "size1") }
+      let(:size2) { FactoryBot.create(:size, size: "size2") }
     
     it 'size_typesとcategoriesは1対多のアソシエーションである1' do
-      expect(category1.size_type).to eq size_type
+      expect(size_type).to be_valid
+      expect(category1.size_type.size_type).to eq "AdultWearSize"
     end
     it 'size_typesとcategoriesは1対多のアソシエーションである2' do
-      expect(size_type.categories).to eq [category1, category2]
+      size_type.categories.each do |category|
+        expect(category.category).to eq "テストカテゴリー1"
+      end
     end
     it 'size_typesとsizesは1対多のアソシエーションである1' do
-      expect(size1.size_type).to eq size_type
+      expect(size1.size_type.size_type).to eq "AdultWearSize"
     end
     it 'size_typesとsizesは1対多のアソシエーションである2' do
-      expect(size_type.sizes).to eq [size1, size2]
+      size_type.sizes.each do |size|
+        expect(size.size).to eq "XXS以下"
+      end
     end
   end
 end
