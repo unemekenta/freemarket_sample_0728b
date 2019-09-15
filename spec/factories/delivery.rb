6f31f7c03d23ae778d4c7d6c349678d0dc5e3405
uneme <unemekenta@gmail.com>
2019-09-15 20:03:53 +0900

    set_factories_delivery

---
+++ spec/factories/delivery.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+
+    factory :delivery do
+      id    {"1"}
+      shipping_fee {"着払い(購入者負担)"}
+      created_at                  {"2019-09-09"}
+      updated_at                  {"2019-09-09"}
+    end
+  
+  end
