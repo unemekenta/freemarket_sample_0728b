baab5e7e6c0a1c910f1f3726aa13de03b6c35618
kamayatsu <50704071+kamayatsu@users.noreply.github.com>
2019-09-21 20:30:48 +0900

    update test product_image model
    associationを削除し、product_image modelのテストを拡充させた。

--- spec/factories/product_images.rb
+++ spec/factories/product_images.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do 
   factory :product_image do
     id {"1"}
-    image {"test.jpg"}
-
-    association :product
-    product {product_image.product}
+    image {File.open("#{Rails.root}/spec/fixtures/lake.jpg")}
+    product_id {"1"}
+    created_at {"2019-09-15"}
+    updated_at {"2019-09-15"}
   end
 end
