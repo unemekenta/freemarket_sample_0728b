9e2a762e77050949e0bfd7c3bf4cfd4efd67f574
Masatomo Sugai <tech.masatomo.s@gmail.com>
2019-09-21 09:12:04 +0900

    fix rails helper(omniauth config)

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -30,7 +30,7 @@ rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
 end
-OmniAuth.config.test_mode = true
+
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
@@ -65,4 +65,5 @@ RSpec.configure do |config|
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.include ControllerMacros, type: :controller
   config.include OmniauthMocks
+  OmniAuth.config.test_mode = true
 end
