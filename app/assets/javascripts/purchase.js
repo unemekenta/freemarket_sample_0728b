dff5d8f29d833a9cce02e1611fd70ca330fddbf3
YuichiMotomura <yu1liveinwellisle@gmail.com>
2019-09-23 22:21:59 +0900

    Update purchase.js
    ポップアップが出たら、購入ボタンは押せないようにした

--- app/assets/javascripts/purchase.js
+++ app/assets/javascripts/purchase.js
@@ -3,8 +3,7 @@ document.addEventListener('turbolinks:load', function(){
     $('#purchase_btn').click(function(){
       setTimeout(function(){
         $('#done_overlay').fadeIn();
-        $(this).css('background-color', '#ccc').css('border', 'none').css('cursor', 'not-allowed');
-        $('#purchase_btn').css('background-color', '$main_gray');
+        $('#purchase_btn').css('background-color', '#ccc').css('border', 'none').css('cursor', 'not-allowed').css('background-color', '$main_gray');
       },1000);
     });
     $('#done_overlay_ok').click(function(){
