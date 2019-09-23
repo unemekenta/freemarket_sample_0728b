bedc763c862e2b8cdec242d85ecd96ec8ea4e96f
YuichiMotomura <yu1liveinwellisle@gmail.com>
2019-09-23 21:47:23 +0900

    Update purchase.js
    購入成功後のポップアップを画面中央に配置されるようにした

--- app/assets/javascripts/purchase.js
+++ app/assets/javascripts/purchase.js
@@ -1,9 +1,26 @@
-$(function(){
-  $('#purchase_btn').click(function(){
-    $('#done_overlay').fadeIn();
-  });
-  $('#done_overlay_ok').click(function(){
-    $('#done_overlay').hide();
-  });
+document.addEventListener('turbolinks:load', function(){
+  $(function(){
+    $('#purchase_btn').click(function(){
+      $('#done_overlay').fadeIn();
+      $(this).css('background-color', '#ccc').css('border', 'none').css('cursor', 'not-allowed');
+      $('#purchase_btn').css('background-color', '$main_gray');
+    });
+    $('#done_overlay_ok').click(function(){
+      $('#done_overlay').hide();
+    });
+    locateCenter();
+    $(window).resize(locateCenter);
 
+    function locateCenter() {
+      let w = $(window).width();
+      let h = $(window).height();
+
+      let dw = $('#done_overlay').outerWidth();
+      let dh = $('#done_overlay').outerHeight();
+
+      $('#done_overlay').css({
+        'left': ((w - dw) / 2) + 'px'
+      });
+    }
+  });
 });
