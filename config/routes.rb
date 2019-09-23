d97eb8d43e8f6fe9240f0ac661cf439305d13c7e
uneme <unemekenta@gmail.com>
2019-09-23 20:24:33 +0900

    fix_routes

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,7 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :users, only: [:show, :new, :create, :edit, :update ] do
+  resources :users do
     resources :deliveraddresses, only: [:new, :create, :edit, :update]
     resources :credit_card, only: [:new, :show] do
       collection do
@@ -35,7 +35,6 @@ Rails.application.routes.draw do
       get 'sales'
       get 'point'
       get 'profile'
-      get 'update'
       get 'email_password'
       get 'purchased'
       get 'forsell'
