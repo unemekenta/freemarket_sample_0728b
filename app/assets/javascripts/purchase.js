ffa0576253805239772cda8d9b72a0d9f33d9801
YuichiMotomura <yu1liveinwellisle@gmail.com>
2019-09-24 00:40:10 +0900

    Update purchase.js
    popup表示時、popup以外の要素はクリック出来ないようにした

--- app/assets/javascripts/purchase.js
+++ app/assets/javascripts/purchase.js
@@ -4,6 +4,7 @@ document.addEventListener('turbolinks:load', function(){
       setTimeout(function(){
         $('#done_overlay').fadeIn();
         $('.purchase_btn_for_popup').css('background-color', '#ccc').css('border', 'none').css('cursor', 'not-allowed').css('background-color', '$main_gray');
+        $('.single-container__header,.purchase-container__product,.purchase-container__deliver,.purchase-container__payment__inner').css('pointer-events', 'none');
       },1000);
     });
     $('#done_overlay_ok').click(function(){
